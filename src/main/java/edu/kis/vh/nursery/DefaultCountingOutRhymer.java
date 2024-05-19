a01f86da685e391a8229d130dd9eeb6c4f90d370
bkapturowski <kaptur2601@gmail.com>
2024-05-19 14:39:01 +0200

    Zastąpienie deklaracji stałymi, dodanie modyfikatora final i uzycie annotacji Override

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,11 +3,11 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
     public static final  int ARRAY_SIZE = 12;
-    public static final int TOTAL = -1;
+    public static final int UNMODIFIED_TOTAL = -1;
 
     private final int[] numbers = new int[ARRAY_SIZE];
 
-    public int total = TOTAL;
+    public int total = UNMODIFIED_TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
@@ -15,11 +15,11 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == UNMODIFIED_TOTAL;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ARRAY_SIZE-1;
     }
 
     protected int peekaboo() {
